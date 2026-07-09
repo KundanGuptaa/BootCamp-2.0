3a12f49604281bd01f4d8c7daed633705dd230bf
KundanGuptaa <kundankumarsobh@gmail.com>
2026-07-09 14:18:12 +0530

    Symetric Number Pyramid

--- 9DayCoding/Day_1/pattern06.java
+++ 9DayCoding/Day_1/pattern06.java
@@ -2,11 +2,15 @@ public class pattern06 {
     public static void main(String[] args){
         int s=5;
         for(int i=1;i<=s;i++){
+            int count=1;
             for(int k=1;k<=s-i;k++){
                 System.out.print(" ");
             }
-            for(int j=1;j<=(2*i-1);j++){
-                System.out.print("*");
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j);
             }
             System.out.println();
         }
